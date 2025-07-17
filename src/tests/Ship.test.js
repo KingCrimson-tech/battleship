59fbb45865767c10d327e21699229742d1762477
Chinmay <chinmayrpatil123@gmail.com>
2025-07-17 20:36:53 +0530

    Ship object implemented with test

---
+++ src/tests/Ship.test.js
@@ -0,0 +1,22 @@
+import Ship from "../models/Ship";
+
+describe("Ship", () => {
+  it("should be created with the correct length", () => {
+    const ship = new Ship(3);
+    expect(ship.length).toBe(3);
+  });
+
+  it("should increase the number of hits when hit", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  it("should be sunk when the number of hits is equal to the length", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
